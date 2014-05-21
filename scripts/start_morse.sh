392dd0eacae4e084abdc55645e6b2f426c44f576
Harmish Khambhaita <harmish.khambhaita@laas.fr>
2014-05-21 18:00:19 +0200

    removing non relevant things from startup script and adding MORSE_RESOURCE_PATH variable

--- scripts/start_morse.sh
+++ scripts/start_morse.sh
@@ -1,38 +1,7 @@
 #! /usr/bin/env sh
 
-# display the ROS_PACKAGE_PATH
-if [ -n "$ROS_PACKAGE_PATH" ]
-then
-  echo "Using ROS_PACKAGE_PATH: $ROS_PACKAGE_PATH"
-fi
-
-#TODO: check if morse and other components are installed and paths are set properly
-
-# set python version for morse
-PYTHON_VERSION=python3.2
-if [ -z "$PYTHON3_EXECUTABLE" ]; then
-    if which python3.2; then
-        PYTHON3_EXECUTABLE=`which python3.2`
-    else
-        echo "Could not find python3 executable. Please set the environment variable PYTHON3_EXECUTABLE to point to it."
-        exit 1
-    fi
-fi
-
-#PYTHON_INST_DIR=`$PYTHON3_EXECUTABLE -c "import distutils.sysconfig, sys; sys.stdout.write(distutils.sysconfig.get_python_lib(0,0,''))"`
-#export PYTHONPATH=$PYYAML_DIR/pyyaml/lib/python3.2/site-packages:$MORSE_DIR/morse/$PYTHON_INST_DIR:$ROSLIB_DIR/src:/usr/$PYTHON_INST_DIR
-
-echo "Using PYTHONPATH: $PYTHONPATH"
-
-if [ -n "$MORSE_BLENDER" ]
-then
-  echo "Using MORSE_BLENDER: $MORSE_BLENDER"
-else
-  echo MORSE_BLENDER path-variable not set
-  exit
-fi
+# add resources of current package to morse
+export MORSE_RESOURCE_PATH=$MORSE_RESOURCE_PATH:`rospack find morse_ros`/blender_files
 
 # start morse with given scenario
-#morse check
 morse run -g 1280x720 $1
-
