9888b228d6d28a5ce6225985f8005cdf136e3ca8
Harmish Khambhaita <hkhambha@harmish.laas.fr>
2014-08-07 14:19:44 +0200

    adding teleport actuator for human

--- morse_ros/scenarios/human_laas_adream_scenario.py
+++ morse_ros/scenarios/human_laas_adream_scenario.py
@@ -9,11 +9,16 @@ pose = Pose()
 human.append(pose)
 pose.add_interface('ros')
 
+# new instance of the  teleport actuator for the human
+teleport = Teleport()
+human.append(teleport)
+teleport.add_interface('ros')
+
 # set the environment to laas_adream
 env = Environment('laas_adream.blend')
 env.set_camera_location([18.0, 4.0, 10.0])
 env.set_camera_rotation([1.0, 0.0 , 1.57])
 
 # put the human in better places
-human.translate(x=3.0, y=4.0, z=0.0)
+human.translate(x=2.0, y=2.0, z=0.0)
 human.rotate(x=0.0, y=0.0, z=0.0)
