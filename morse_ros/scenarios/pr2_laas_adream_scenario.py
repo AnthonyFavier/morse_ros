61d16a997886fbe59ba43f5be901a621602dfd15
Harmish Khambhaita <hkhambha@harmish.laas.fr>
2014-08-07 14:20:29 +0200

    removing default roation of the pr2

--- morse_ros/scenarios/pr2_laas_adream_scenario.py
+++ morse_ros/scenarios/pr2_laas_adream_scenario.py
@@ -10,5 +10,5 @@ env.set_camera_location([18.0, 4.0, 10.0])
 env.set_camera_rotation([1.0, 0.0 , 1.57])
 
 # put the robot in better place
-pr2.rotate(x=0.0, y=0.0, z=3.0)       # for roation check on the pr2
+pr2.rotate(x=0.0, y=0.0, z=0.0)       # for roation check on the pr2
 pr2.translate(x=2.0, y=2.0, z=0.0)    # put pr2 in some good place
