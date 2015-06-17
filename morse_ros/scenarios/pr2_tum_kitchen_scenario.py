9be942c862301cfc05456b740b6edbeaceef4381
harmish khambhaita <harmish@laas.fr>
2015-06-17 19:34:15 +0200

    fixing tum_kitchen scenarios
    adding non-human scenario
    adding launch files accordingly

--- morse_ros/scenarios/pr2_tum_kitchen_scenario.py
+++ morse_ros/scenarios/pr2_tum_kitchen_scenario.py
@@ -5,15 +5,6 @@ from morse.builder import *
 pr2 = NavPR2()
 pr2.add_interface("ros")
 
-# adding human model
-human = Human()
-human.properties(WorldCamera = True)
-
-# pose sensor for the human
-pose = Pose()
-human.append(pose)
-pose.add_interface('ros')
-
 # set the environment to tum_kitchen
 env = Environment('tum_kitchen.blend')
 env.set_camera_location([10.0, -10.0, 10.0])
@@ -22,5 +13,3 @@ env.set_camera_rotation([1.0470, 0, 0.7854])
 # put the robot and human in better places
 pr2.rotate(x=0.0, y=0.0, z=0.0)       # for roation check on the pr2
 pr2.translate(x=2.5, y=3.2, z=0.0)    # put pr2 in some good place
-human.translate(x=6.0, y=0.7, z=0.0)
-human.rotate(x=0.0, y=0.0, z=-3.0)
---
+++ morse_ros/scenarios/tum_kitchen_scenario.py
@@ -0,0 +1,6 @@
+from morse.builder import *
+
+# set the environment to tum_kitchen
+env = Environment('tum_kitchen.blend')
+env.set_camera_location([10.0, -10.0, 10.0])
+env.set_camera_rotation([1.0470, 0, 0.7854])
