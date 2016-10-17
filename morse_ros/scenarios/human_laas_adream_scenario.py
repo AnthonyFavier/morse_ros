533afbe7550d96b667e2f72206a1267378014e43
harmish khambhaita <harmish@laas.fr>
2016-10-17 18:11:07 +0200

    adding teleport actuator to robots by default fixing clock topic naming some code-formatting

--- morse_ros/scenarios/human_laas_adream_scenario.py
+++ morse_ros/scenarios/human_laas_adream_scenario.py
@@ -7,25 +7,24 @@ human.properties(WorldCamera = True)
 # pose sensor for the human
 pose = Pose()
 human.append(pose)
-pose.add_interface('ros')
+pose.add_interface("ros", topic="human_pose")
 
 # new instance of the  teleport actuator for the human
 teleport = Teleport()
 human.append(teleport)
-teleport.add_interface('ros')
+teleport.add_interface("ros", topic="human_teleport_pose")
 
 # set the environment to laas_adream
-env = Environment('laas_adream.blend', fastmode=True)
+env = Environment("laas_adream.blend", fastmode=True)
 env.set_camera_location([18.0, 4.0, 10.0])
 env.set_camera_rotation([1.0, 0.0 , 1.57])
 
 # put the human in better places
-human.translate(x=2.0, y=2.0, z=0.0)
-human.rotate(x=0.0, y=0.0, z=0.0)
+teleport.translate(2.0, 2.0, 0.0)
 
 # add clock
 clock = Clock()
-clock.add_stream("ros")
+clock.add_interface("ros", topic="clock")
 human.append(clock)
 
 env.use_relative_time(True)
